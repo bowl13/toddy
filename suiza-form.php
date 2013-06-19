aa52bc97e837598f506c13dc1399d2bdb759270d
bowl13 <mauro@bowl.cl>
2013-06-19 18:41:16 -0400

    update01

---
+++ suiza-form.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Toddy</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="bootstrap-responsive.css">
+
+	<link rel="stylesheet" href="bootstrap/css/suiza_form.css">
+	<link rel="stylesheet" href="css/productos.css">
+	<link rel="stylesheet" href="css/internas.css">
+
+</head>
+<body>
+	
+	<!--botonera toddy-->
+	<div id="botonera_int">
+		<div id="navegacion_int">
+			<div id="mover">
+				<a href="index.html">
+					<img border="0" src="images/logo-int-trans.png" style="filter:progid:DXImageTransform.Microsoft.AlphaImageLoader (src='imgaes/logo-int-trans',sizingMethod='scale');" />
+				</a>
+				<a href="productos.html" id="btn1"></a>
+				<a href="juegos.html" id="btn2"></a>
+				<a href="comics.html" id="btn3"></a>
+				<div id="share">
+					<a href="http://twitter.com/toddy_cl" target="_blank" id="share1"></a>
+					<a href="http://www.facebook.com/ToddyChile" target="_blank" id="share2"></a>
+					<a href="http://www.youtube.com/user/toddychile?feature=watch" target="_blank" id="share3"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!--cont form-->
+	<div class="container-fluid" id="wrap">
+		
+			<div class="row-fluid">
+		
+				<div class="span6 offset3">header</div>
+		
+			</div>
+
+			<div class="row-fluid">
+				<div class="span6 offset3">
+					
+					<form action="">
+						<fildset>
+							<div class="control-group">
+								<label class="control-label" for="inputnombre">NOMBRE</label>
+								<div class="controls">
+									<input type="text" id="inputEmail" placeholder="nombre">
+								</div>
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputRut">rut</label>
+								<div class="controls"><input type="text" placeholder="Rut"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputMail">e-mail</label>
+								<div class="controls"><input type="text" placeholder="Email"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputNac">fecha de nacimieneto</label>
+								<div class="controls"><input type="text" placeholder="Fecha de Nacimiento"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="contrl-label" for="inputTelefono">telefono</label>
+								<div class="controls"><input type="text" placeholder="telefono"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputDirec">direccion</label>
+								<div class="controls"><input type="text" placeholder="Direccion"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputComuna">comuna</label>
+								<div class="controls"><input type="text" placeholder="Comuna"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputRegion">region</label>
+								<div class="controls"><input type="text" placeholder="Region"></div>
+								
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="inputDato">dato</label>
+								<div class="controls"><input type="text" placeholder="dato"></div>
+
+							</div>
+
+							<div class="control-group">
+								<div class="controls">
+									<laber class="checkbox control-label">
+									<input type="checkbox">
+										soy mayor de 18 años
+									</laber>
+								</div>
+								
+							</div>
+							
+							<div class="control-group">
+								<input type="submit">
+							</div>
+
+						</fildset>
+					</form>
+
+				</div>
+			</div>
+
+		<div class="push"></div>
+
+	</div>
+
+	<footer class="footer-fix container-fluid text-center">® Copyright PepsiCo 2013 - Todos los derechos reservados.</footer>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
