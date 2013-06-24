ae2475b7f6171d802cb2ef79fb165928f5ac379b
bowl13 <mauro@bowl.cl>
2013-06-24 11:34:18 -0400

    update-lunes

--- form.php
+++ form.php
@@ -19,7 +19,9 @@
 
 </head>
 <body>
-	<!--botonera toddy-->
+	<div class="container-fluid">
+		
+		<!--botonera toddy-->
 	<div id="botonera_int">
 		<div id="navegacion_int">
 			<div id="mover">
@@ -38,171 +40,176 @@
 		</div>
 	</div>
 
-		
-	<div class="container-fluid content">
-		
-		<div class="row-fluid">
+
+	<div class="row-fluid" style="margin-top:80px;">
 			
-			<div class="span12 text-center">
+		<div class="span12 text-center">
 			
-				<img class="headers" src="images/tit_suiza.gif" alt="Gana un viaje Suiza">
+			<img class="headers" src="images/tit_suiza.gif" alt="Gana un viaje Suiza">
+			
+			<div class="row-fluid bg-w">
 				
-				<div class="row-fluid bg-w">
-					<div class="span12">
+				<div class="span12">
 						
-						<p>Suiza está muy cerca, sólo debes comprar Toddy pa’ la leche en supermercados Jumbo, llenar este formulario y ya estarás  participando por un viaje para 2 personas!</p>
-						
-						<div class="baca">
-							
-							<img src="images/baca_toddy.gif" alt="Toddy">
+					<p style="margin-bottom:100px;">Suiza está muy cerca, sólo debes comprar Toddy pa’ la leche en supermercados Jumbo, llenar este formulario y ya estarás  participando por un viaje para 2 personas!</p>
 
-						</div>
-						
-						<div class="row-fluid cont-form">
+				</div>
+				<div style="clear:both;"></div>
+				<div class="baca">
+					
+					<img src="images/baca_toddy.gif" alt="">
+
+				</div>
+				
+				<div class="row-fluid cont-form text-left well-small">
+					
+					<form action="">
+					
+						<div class="cont1">
+
+											
+							<div class="control-group">											
+							<label class="control-label pull-left" for="inputnombre">nombre</label>
+							<div class="controls">
+								<input type="text" id="inputEmail" placeholder="Toddy">
+								</div>
+
+							</div>
 							
-							<div class="span12 text-left">
+							<div class="control-group">
+
+								<label class="control-label pull-left" for="inputApellido">apellido</label>
+								<div class="controls">
+									<input type="text" placeholder="Vacuno">
+								</div>
 								
-								<form action="">
+							</div>
+							
+							<div class="control-group">
 
-									<fildset>
+								<label class="control-label pull-left" for="inputRut">rut</label>
+								<div class="controls">
+									<input type="text" placeholder="Rut">
+								</div>
+								
+							</div>
+							
+							<div class="control-group">
+
+								<label class="control-label pull-left" for="inputEmail">e-mail</label>
+								<div class="controls">
+									<input type="text" placeholder="Toddy@vacuno.cl">
+								</div>
+								
+							</div>
+							
+							<div class="control-group">
+
+								<label class="contrl-label pull-left" for="inputCelular">celular</label>
+								<div class="controls">
+									<input type="text" placeholder="Celular">
+								</div>
+								
+							</div>
+							
+							<div class="control-group">
+
+								<label class="control-label pull-left" for="inputDirec">direccion</label>
+								<div class="controls">
+									<input type="text" placeholder="Direccion">
+								</div>
+								
+							</div>
+							
+							<div class="control-group">
+
+								<label class="control-label pull-left" for="inputComuna">comuna</label>
+								<div class="controls">
+									<select>
 										
-										<div class="row-fluid">
+										<option value="santiago">Santiago</option>
 
-											<div class="span6">
-												
-												<div class="control-group">
-											
-													<label class="control-label" for="inputnombre">nombre</label>
-													<div class="controls">
-														<input type="text" id="inputEmail" placeholder="nombre">
-													</div>
+									</select>
+								</div>
+								
+							</div>
+							
+								
+							<div class="control-group">
 
-												</div>
-												
-												<div class="control-group">
-
-													<label class="control-label" for="inputApellido">apellido</label>
-													<div class="controls">
-														<input type="text" placeholder="Rut">
-													</div>
-													
-												</div>
-												
-												<div class="control-group">
-
-													<label class="control-label" for="inputRut">rut</label>
-													<div class="controls">
-														<input type="text" placeholder="Email">
-													</div>
-													
-												</div>
-												
-												<div class="control-group">
-
-													<label class="control-label" for="inputEmail">e-mail</label>
-													<div class="controls">
-														<input type="text" placeholder="Fecha de Nacimiento">
-													</div>
-													
-												</div>
-												
-												<div class="control-group">
-
-													<label class="contrl-label" for="inputCelular">celular</label>
-													<div class="controls">
-														<input type="text" placeholder="telefono">
-													</div>
-													
-												</div>
-												
-												<div class="control-group">
-
-													<label class="control-label" for="inputDirec">direccion</label>
-													<div class="controls">
-														<input type="text" placeholder="Direccion">
-													</div>
-													
-												</div>
-												
-												<div class="control-group">
-
-													<label class="control-label" for="inputComuna">comuna</label>
-													<div class="controls">
-														<select>
-															
-															<option value="santiago">Santiago</option>
-
-														</select>
-													</div>
-													
-												</div>
-												
-													
-												<div class="control-group">
-
-													<label class="control-label" for="inputRegion">region</label>
-													<div class="controls">
-														<select>
-															
-															<option value="metropilitana">RM</option>
-
-														</select>
-													</div>
-													
-												</div>
-											
-												<div class="control-group">
-
-													<label class="control-label" for="inputDato">nº boleta</label>
-													<div class="controls">
-														<input type="text" placeholder="xxxxxx">
-													</div>
-
-												</div>
-
-												<p>Términos y condiciones La información entregada es de uso exclusivo de Evercrisp S.A. Tus datos serán tratados de forma confidencial y bajo ningún motivo serán utilizados con fines ajenos a la promoción, ni entregados a terceros.</p>
-												
-												<div class="control-group">
-
-													<div class="controls">
-														<laber class="checkbox control-label">
-														<input type="checkbox">
-															Declaro ser mayor de 18 años y <a href="">acepto las bases de inscripción.</a>
-														</laber>
-													</div>
-													
-												</div>
-												
-												<div class="control-group">
-
-													<div class="controls">
-														<input type="submit">	
-													</div>
-													
-												</div>
-
-												<p>Promoción valida desde el 27 de Junio al 27 de Agosto del 2013. Bases en notaría Musalem.</p>
-												
-										</div>
+								<label class="control-label pull-left" for="inputRegion">region</label>
+								<div class="controls">
+									<select>
 										
-									</fildset>
-								</form>
+									<option value="metropilitana">RM</option>
+
+									</select>
+								</div>
+								
+							</div>
+											
+						</div>
+
+						<div class="cont2">
+							
+							<div class="control-group">
+
+								<label class="control-label" for="inputDato">nº boleta</label>
+								<div class="controls">
+									<input type="text" placeholder="xxxxxx" style="height:45px;width:90%;" class="text-center">
+								</div>
 
 							</div>
 
-						</div>
-					</div>
-				</div>
+							<p style="margin-bottom:30px;margin-top:20px;">Términos y condiciones La información entregada es de uso exclusivo de Evercrisp S.A. Tus datos serán tratados de forma confidencial y bajo ningún motivo serán utilizados con fines ajenos a la promoción, ni entregados a terceros.</p>
+							
+							<div class="control-group" style="margin-bottom:30px">
 
-			</div>
+								<div class="controls">
+									
+									<input type="checkbox" class="check pull-left">
+									<label class="checkbox control-label pull-left">
+									Declaro ser mayor de 18 años y <a href="">acepto las bases de inscripción.</a>
+									</label>
+
+								</div>
+								
+							</div>
+							
+							<div style="margin-bottom:30px" class="control-group text-center">
+
+								<div class="controls">
+									<input class="btn_parti" type="submit" value="participar">	
+								</div>
+								
+							</div>
+
+							<p style="max-width:270px;padding:15px 0;" class="pull-left">Promoción valida desde el 27 de Junio al 27 de Agosto del 2013. Bases en notaría Musalem.</p>
+							<p class="pull-left">
+								
+								<img style="margin-left:4px" src="images/logo_jumbo.gif" alt="">
+
+							</p>
+
+						</div>
+
+					</form>
+
+				</div>	
+				
+			</div>	
 
 		</div>
-		
+
 	</div>
+
+	</div>
+	
 
 	<footer class="footer-fix container-fluid text-center">® Copyright PepsiCo 2013 - Todos los derechos reservados.</footer>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
+
 </body>
 </html>
